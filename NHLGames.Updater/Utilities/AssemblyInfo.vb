e269ef0697c72d22ae05a4ffc9105594018a05fe
Wallet <joseouellet@gmail.com>
2021-07-01 21:50:22 -0400

    changed version regex

--- NHLGames.Updater/Utilities/AssemblyInfo.vb
+++ NHLGames.Updater/Utilities/AssemblyInfo.vb
@@ -3,9 +3,10 @@
 Public Class AssemblyInfo
     Public Shared Function IsNewerVersionThanCurrent(name As String) As Boolean
         Dim m As Match = Regex.Match(name, "v(\.)?((?:\d+\.)*\d+?)")
+        Dim version = m.Groups(m.Groups.Count - 1)
 
-        If m.Groups(2).Success Then
-            Dim availableVersion As Version = New Version(m.Groups(2).Value)
+        If version.Success Then
+            Dim availableVersion As Version = New Version(version.Value)
             Dim currentVersion As Version
             Try
                 currentVersion = New Version(FileVersionInfo.GetVersionInfo(Updater.NHLGamesFullPath).FileVersion)
